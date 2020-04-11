045b57c8bf01892e7431ccbf943273a54395f294
Kevin Mendez <kevinrmendez@gmail.com>
2020-04-11 13:51:38 +0300

    save last changes

---
+++ lib/apikeys.dart
@@ -0,0 +1,8 @@
+var apikeys = {
+  "appId": "ca-app-pub-7306861253247220~8235596983",
+  // "addMobBanner": "ca-app-pub-7306861253247220/6782720996",
+  // "addMobInterstellar": "ca-app-pub-7306861253247220/3815993914",
+  // "introAdd": "ca-app-pub-7306861253247220/9870338122",
+  // "contactListAdd": "ca-app-pub-7306861253247220/6679461400",
+  "addInterstellarTest": "ca-app-pub-3940256099942544/1033173712",
+};
