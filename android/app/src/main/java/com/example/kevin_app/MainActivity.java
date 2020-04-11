286ec1b4bcdd3f7772fdbc39fc93a1c9014d41de
Kevin Mendez <kevinrmendez@gmail.com>
2020-04-11 14:31:05 +0300

    adds removed

--- android/app/src/main/java/com/example/kevin_app/MainActivity.java
+++ android/app/src/main/java/com/example/kevin_app/MainActivity.java
@@ -4,25 +4,10 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-
-
-import java.io.File;
-import java.io.IOException;
-
-import android.util.DisplayMetrics;
-import android.view.Display;
-
-import 	android.util.Log;
-
-
-public class MainActivity extends FlutterActivity  {
-
- 
+public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
-
   }
-
 }
