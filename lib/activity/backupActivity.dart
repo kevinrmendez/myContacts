4bc92f2dfbd97b56f4aa5921d599197965f13316
Kevin Mendez <kevinrmendez@gmail.com>
2020-05-07 00:01:46 +0300

    send backup files added

--- lib/activity/backupActivity.dart
+++ lib/activity/backupActivity.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:kevin_app/utils/utils.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -16,7 +17,8 @@ class _BackupActivityState extends State<BackupActivity> {
   Directory dir;
   String path;
   List files = [];
-  List myFiles;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final snackBar = (text) => SnackBar(content: Text(text));
 
   @override
   void initState() {
@@ -27,26 +29,68 @@ class _BackupActivityState extends State<BackupActivity> {
   _getFiles() async {
     dir = await getExternalStorageDirectory();
     path = dir.absolute.path;
-    files = dir.listSync();
 
     setState(() {
-      myFiles = files;
+      //list of file paths
+      files = dir.listSync().map((f) => f.path).toList();
     });
     files.forEach(((f) => print("FILE $f")));
   }
 
-  _deleteFile(String path) {
-    final dir = Directory(path);
-    dir.deleteSync(recursive: true);
+  _deleteFile(String pathFile) async {
+    String fileName = pathFile.split('/').last;
+    print("FILENAME: $fileName");
+    Directory dir = await getExternalStorageDirectory();
+    String path = dir.absolute.path;
+    File file = File("${path}/${fileName}");
+    print("$path/$fileName");
+    // File file = File('$path/contact.vcf');
+    file.delete();
+  }
+
+  _sendFile(String pathFile) async {
+    Directory dir = await getExternalStorageDirectory();
+    String fileName = pathFile.split('/').last;
+
+    String path = dir.absolute.path;
+    String filePath = "$path/$fileName";
+    final Email email = Email(
+      body: translatedText("text", context),
+      subject: translatedText("email_csv_subject", context),
+      // recipients: ['example@example.com'],
+      attachmentPaths: [filePath],
+    );
+    await FlutterEmailSender.send(email);
+    _scaffoldKey.currentState.showSnackBar(
+        snackBar(translatedText("snackbar_contact_exported", context)));
   }
 
   List<Widget> _buildListFiles() {
-    return myFiles.map((file) {
+    return files.map((file) {
+      String fileName = file.split('/').last;
+
       return ListTile(
-        title: Text(file.toString()),
-        trailing: IconButton(
-          icon: Icon(Icons.delete),
-          onPressed: () {},
+        title: Text(fileName),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                _deleteFile(file.toString());
+                files.remove(file);
+                setState(() {
+                  files = files;
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () {
+                _sendFile(file);
+              },
+            ),
+          ],
         ),
       );
     }).toList();
@@ -62,9 +106,13 @@ class _BackupActivityState extends State<BackupActivity> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text('backup'),
-              Column(
-                children: myFiles.length == 0 ? SizedBox() : _buildListFiles(),
-              )
+              Column(children: [
+                files.length == 0
+                    ? SizedBox()
+                    : Column(
+                        children: _buildListFiles(),
+                      )
+              ])
             ],
           ),
         ),
