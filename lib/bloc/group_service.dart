5dce045752270c2cda173d1c204272f707df0bc7
Kevin Mendez <kevinrmendez@gmail.com>
2020-07-24 09:34:58 +0300

    add group bloc service

--- lib/bloc/group_service.dart
+++ lib/bloc/group_service.dart
@@ -45,18 +45,18 @@ class GroupService {
     return id;
   }
 
-  // void orderFoodsAscending() {
-  //   List<Protein> orderList = currentList;
-  //   orderList.sort((a, b) => a.name.compareTo(b.name));
-  //   _proteinList.add(orderList);
-  // }
+  void orderGroupsAscending() {
+    List<Group> orderList = currentList;
+    orderList.sort((a, b) => a.name.compareTo(b.name));
+    _groupList.add(orderList);
+  }
 
-  // void orderFoodsDescending() {
-  //   List<Protein> orderList = currentList;
-  //   orderList.sort((a, b) => a.name.compareTo(b.name));
-  //   List<Protein> reversedList = orderList.reversed.toList();
-  //   _proteinList.add(reversedList);
-  // }
+  void orderGroupsDescending() {
+    List<Group> orderList = currentList;
+    orderList.sort((a, b) => a.name.compareTo(b.name));
+    List<Group> reversedList = orderList.reversed.toList();
+    _groupList.add(reversedList);
+  }
 }
 
 GroupService groupService = GroupService();
